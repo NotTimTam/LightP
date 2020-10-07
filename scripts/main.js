0cecc643c6018effe3b0906de8856347489bbcc2
Tim Phillips <timmyiscool8@gmail.com>
2020-10-06 22:28:40 -0700

    Added more features.

--- scripts/main.js
+++ scripts/main.js
@@ -21,3 +21,27 @@ function setUrl() {
     document.execCommand('insertHTML', false, '<a href="' + url + '" target="_blank">' + sText + '</a>');
     document.getElementById('txtFormatUrl').value = '';
 }
+
+// Print feature.
+function print()
+{
+    var prtContent = document.getElementById("page");
+    console.log(prtContent)
+    var WinPrint = window.open('', '', 'letf=100,top=100,width=600,height=600');
+    WinPrint.document.write(prtContent.innerHTML);
+    WinPrint.document.close();
+    WinPrint.focus();
+    WinPrint.print();
+    WinPrint.close()   
+}
+
+// Color feature.
+function changeForeColor() {
+    color = document.getElementById("cp").value;
+    format("foreColor", color);
+}
+
+function changeBGColor() {
+    color = document.getElementById("bcp").value;
+    format("backColor", color);
+}
